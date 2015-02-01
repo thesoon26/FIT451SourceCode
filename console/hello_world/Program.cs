ecf0f4984287885d6b96d83d75da84925fc1fb93
HA Quang Minh <minhprg@gmail.com>
2015-02-01 22:56:52 +0700

    Updates for hello world and falling rocks, week 3

--- console/hello_world/Program.cs
+++ console/hello_world/Program.cs
@@ -7,6 +7,37 @@ namespace hello_world
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
+
+			Person person = new Person ("Minh");
+			person.PrintMyName ();
+			person.DivideMyAge (0);
+		}
+	}
+
+	class Person
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+
+		public Person(string name)
+		{
+			this.Name = name;
+		}
+
+		public void PrintMyName() 
+		{
+			Console.WriteLine ("My name is: " + this.Name);
+		}
+
+		public void DivideMyAge(int number)
+		{
+			try {
+				this.Age = this.Age / number;
+				Console.WriteLine("My age is : " + this.Age);
+			}
+			catch (Exception ex) {
+				Console.Write ("Error:" + ex.Message);
+			}
 		}
 	}
 }

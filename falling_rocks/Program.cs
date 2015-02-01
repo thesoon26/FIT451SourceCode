788d43a9a9ce293e668408a7bc08aa40fb6750e5
HA Quang Minh <minhprg@gmail.com>
2015-02-01 22:21:56 +0700

    First commit

---
+++ falling_rocks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace falling_rocks
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ helloworld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace helloworld
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.ReadLine ();
+		}
+	}
+}
